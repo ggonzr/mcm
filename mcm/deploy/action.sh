7847cfd288cbf4c311dfd9fc2768f819c3301e02
Geovanny Gonzalez-Rodriguez <geovanny.gonzalez@cern.ch>
2024-02-27 00:10:12 +0100

    Debug: Decompression

--- mcm/deploy/action.sh
+++ mcm/deploy/action.sh
@@ -17,7 +17,7 @@ echo 'Downloading McM data....'
 MCM_DATA_FILE="$HOME/data.tar.gz"
 curl -s -o $MCM_DATA_FILE $MCM_EXAMPLE_DATA_URL
 echo 'Decompressing ....'
-tar -xzf $MCM_DATA_FILE
+tar -xzvf $MCM_DATA_FILE
 ls -alrh $HOME
 
 echo 'Creating data folders'
